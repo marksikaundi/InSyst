33e656545d1ad074c04c6f9085a5bf518e9c132a
marksikaundi <emmanuelmark117@gmail.com>
2025-01-01 16:02:02 +0200

    refactor: simplify Navbar component by removing dropdown menus and enhancing link navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,7 @@ import Link from "next/link"
 import { Menu, X } from 'lucide-react'
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = React.useState(false)
@@ -28,52 +23,20 @@ export default function Navbar() {
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex md:items-center md:space-x-8">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center text-gray-300 hover:text-white">
-                Features
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>Feature 1</DropdownMenuItem>
-                <DropdownMenuItem>Feature 2</DropdownMenuItem>
-                <DropdownMenuItem>Feature 3</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            
 
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center text-gray-300 hover:text-white">
-                Company
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>About</DropdownMenuItem>
-                <DropdownMenuItem>Careers</DropdownMenuItem>
-                <DropdownMenuItem>Contact</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+           
 
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center text-gray-300 hover:text-white">
-                Resources
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>Blog</DropdownMenuItem>
-                <DropdownMenuItem>Guides</DropdownMenuItem>
-                <DropdownMenuItem>Help Center</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <Link href="/features" className="text-gray-300 hover:text-white">
+              Features
+            </Link>
 
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center text-gray-300 hover:text-white">
-                Docs
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>Getting Started</DropdownMenuItem>
-                <DropdownMenuItem>API Reference</DropdownMenuItem>
-                <DropdownMenuItem>Examples</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <Link href="/contribute" className="text-gray-300 hover:text-white">
+              Contribute
+            </Link>
 
-            <Link href="/pricing" className="text-gray-300 hover:text-white">
-              Pricing
+            <Link href="/about" className="text-gray-300 hover:text-white">
+              About
             </Link>
           </div>
 
@@ -82,7 +45,7 @@ export default function Navbar() {
             <Button variant="ghost" className="text-gray-300 hover:text-white">
               Sign in
             </Button>
-            <Button variant="secondary">Get Started</Button>
+            <Button variant="secondary">Try now</Button>
           </div>
 
           {/* Mobile menu button */}
