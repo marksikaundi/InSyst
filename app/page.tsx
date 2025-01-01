74c4a5ea349bae2edad3c248946c420803cd7db8
marksikaundi <emmanuelmark117@gmail.com>
2025-01-01 15:20:42 +0200

    fix: update greeting message in Home component for improved clarity

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello there</h2>
+      <h2>{"Hello there!"}</h2>
     </main>
   )
 }
