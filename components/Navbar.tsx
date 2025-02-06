a1ca084e32be02338b71fbdde964ee560c047922
marksikaundi <emmanuelmark117@gmail.com>
2025-02-06 14:59:26 +0000

    🖌️ Update Navbar background color and modify navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Nav() {
   return (
-    <div className="bg-[#2D1537] ">
+    <div className="bg-black ">
       <header className="container mx-auto px-4">
         <nav className="flex items-center justify-between py-4">
           <div className="flex items-center gap-8">
@@ -16,48 +16,21 @@ export default function Nav() {
                 className="h-12 w-auto"
               />
             </Link>
-            <div className="hidden md:flex items-center gap-6">
-              <Link href="/programs" className="text-white/80 hover:text-white">
-                Programs
-              </Link>
-              <Link
-                href="/challenges/frontend"
-                className="text-white/80 hover:text-white"
-              >
-                Challenges
-              </Link>
-              <Link
-                href="/resources"
-                className="text-white/80 hover:text-white"
-              >
-                Resources
-              </Link>
-              <Link
-                href="/contributing"
-                className="text-white/80 hover:text-white"
-              >
-                Contributing
-              </Link>
-            </div>
+            
           </div>
           <div className="flex items-center gap-4">
             <Link
               href="/premium"
               className="text-white/80 hover:text-white hidden md:block"
             >
-              Premium
+              Contribute
             </Link>
-            <Link
-              href="https://wa.me/+260776578583"
-              className="hidden md:inline-flex h-9 items-center justify-center rounded-md border border-white  px-4 text-sm text-white"
-            >
-              Talk to us
-            </Link>
+            
             <Link
               href="#"
               className="inline-flex h-9 items-center justify-center rounded-md bg-white px-4 text-sm font-medium text-[#2D1537] hover:bg-white/90"
             >
-              Mentorship
+              Try now free
             </Link>
           </div>
         </nav>
